7ead0ed6426df51e88e11e333aef2b36315c2ed2
Paul Pessoa <paulmspessoa@gmail.com>
2022-02-20 11:59:07 -0300

    descrição produtos

--- assets/script.js
+++ assets/script.js
@@ -2,18 +2,29 @@ console.log("conectado");
 
 
 function selecionaTemaki() {
-    document.getElementById('temaki').style.borderColor = 'green';
- 
+    document.getElementById('temaki').style.borderColor = '#00c943';
+    document.getElementById('acai').style.borderColor = 'white'; 
+    document.getElementById('pizza').style.borderColor = 'white'; 
+    document.getElementById('combo').style.borderColor = 'white'; 
 };
 
 function selecionaAcai() {
-    document.getElementById('acai').style.borderColor = 'green';
+    document.getElementById('acai').style.borderColor = '#00c943';
+    document.getElementById('temaki').style.borderColor = 'white'; 
+    document.getElementById('pizza').style.borderColor = 'white'; 
+    document.getElementById('combo').style.borderColor = 'white';
 };
 
 function selecionaCombo() {
-    document.getElementById('combo').style.borderColor = 'green';
+    document.getElementById('combo').style.borderColor = '#00c943';
+    document.getElementById('acai').style.borderColor = 'white'; 
+    document.getElementById('pizza').style.borderColor = 'white'; 
+    document.getElementById('temaki').style.borderColor = 'white';
 };
 
 function selecionaPizza() {
-    document.getElementById('pizza').style.borderColor = 'green';
+    document.getElementById('pizza').style.borderColor = '#00c943';
+    document.getElementById('acai').style.borderColor = 'white'; 
+    document.getElementById('temaki').style.borderColor = 'white'; 
+    document.getElementById('combo').style.borderColor = 'white';
 };
