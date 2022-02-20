1acc29b6fc940c8c504189c5fe011ef3d4195d5f
Paul Pessoa <paulmspessoa@gmail.com>
2022-02-20 01:45:13 -0300

    funcao selecionar produtos

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,19 @@
 console.log("conectado");
+
+
+function selecionaTemaki() {
+    document.getElementById('temaki').style.borderColor = 'green';
+ 
+};
+
+function selecionaAcai() {
+    document.getElementById('acai').style.borderColor = 'green';
+};
+
+function selecionaCombo() {
+    document.getElementById('combo').style.borderColor = 'green';
+};
+
+function selecionaPizza() {
+    document.getElementById('pizza').style.borderColor = 'green';
+};
