488424ff0daa25b52eef90eb103dd0cc01647629
Paul Pessoa <paulmspessoa@gmail.com>
2022-02-20 23:55:59 -0300

    recusou whatsapp

--- assets/script.js
+++ assets/script.js
@@ -1,68 +1,69 @@
 console.log("conectado");
 var prato = "";
 function selecionaTemaki() {
-/*  document.getElementById('temaki').style.borderColor = '#00c943'; adiciona borda verde*/
-    prato = document.getElementById('titulo-temaki').textContent; 
-    document.getElementById('compra-temaki').style.backgroundColor = "#red"; /*muda cor do botao*/
-    document.getElementById('compra-temaki').textContent = "( x ) Remover";/*muda texto do botao*/
+  /*  document.getElementById('temaki').style.borderColor = '#00c943'; adiciona borda verde*/
+  prato = document.getElementById("titulo-temaki").textContent;
+  document.getElementById("compra-temaki").style.backgroundColor =
+    "#red"; /*muda cor do botao*/
+  document.getElementById("compra-temaki").textContent =
+    "( x ) Remover"; /*muda texto do botao*/
 
-    var botao = document.getElementById('compra-temaki');/*criar cariável para botao*/
-    botao.setAttribute("onclick","removerSelecionado();");/*troca a função*/
-};
+  var botao =
+    document.getElementById("compra-temaki"); /*criar cariável para botao*/
+  botao.setAttribute("onclick", "removerSelecionado();"); /*troca a função*/
+}
 function removerSelecionado() {
-    prato = ""; 
-    document.getElementById('temaki').style.borderColor = 'white';
-    document.getElementById('compra-temaki').style.backgroundColor = "red";
-    document.getElementById('compra-temaki').textContent = "Escolher";
-    
-    var botao = document.getElementById('compra-temaki');
-    botao.setAttribute("onclick","selecionaTemaki();");
-};
+  prato = "";
+  document.getElementById("temaki").style.borderColor = "white";
+  document.getElementById("compra-temaki").style.backgroundColor = "red";
+  document.getElementById("compra-temaki").textContent = "Escolher";
 
-
-
-
-
-
-
-
-
+  var botao = document.getElementById("compra-temaki");
+  botao.setAttribute("onclick", "selecionaTemaki();");
+}
 
 function selecionaAcai() {
-    let prato = "Temaki de Salmão"; 
-    document.getElementById('acai').style.borderColor = '#00c943';
-    document.getElementById('temaki').style.borderColor = 'white'; 
-    document.getElementById('pizza').style.borderColor = 'white'; 
-    document.getElementById('combo').style.borderColor = 'white';
-};
+  let prato = "Temaki de Salmão";
+  document.getElementById("acai").style.borderColor = "#00c943";
+  document.getElementById("temaki").style.borderColor = "white";
+  document.getElementById("pizza").style.borderColor = "white";
+  document.getElementById("combo").style.borderColor = "white";
+}
 
 function selecionaCombo() {
-    document.getElementById('combo').style.borderColor = '#00c943';
-    document.getElementById('acai').style.borderColor = 'white'; 
-    document.getElementById('pizza').style.borderColor = 'white'; 
-    document.getElementById('temaki').style.borderColor = 'white';
-};
+  document.getElementById("combo").style.borderColor = "#00c943";
+  document.getElementById("acai").style.borderColor = "white";
+  document.getElementById("pizza").style.borderColor = "white";
+  document.getElementById("temaki").style.borderColor = "white";
+}
 
 function selecionaPizza() {
-    document.getElementById('pizza').style.borderColor = '#00c943';
-    document.getElementById('acai').style.borderColor = 'white'; 
-    document.getElementById('temaki').style.borderColor = 'white'; 
-    document.getElementById('combo').style.borderColor = 'white';
-};
+  document.getElementById("pizza").style.borderColor = "#00c943";
+  document.getElementById("acai").style.borderColor = "white";
+  document.getElementById("temaki").style.borderColor = "white";
+  document.getElementById("combo").style.borderColor = "white";
+}
 
-
-
+var boraZap;
 
 function realizarPedido() {
-    var boraZap = confirm("Você será redirecionado para o nosso WhatsApp. Tudo bem?");
-    let mensagem;
-    mensagem = "Boa noite, gostaria de pedir um " + prato + ".";
- 
-    if (prato === "") {
-        alert("Escolha um produto...");
-    }
-    else if (boraZap == true) {
-        boraZap
-        window.open("https://wa.me/5581995097377?text=" + mensagem);
-    }    
+  let mensagem;
+  mensagem = "Boa noite, gostaria de pedir um " + prato + ".";
+
+  if (prato === "") {
+    alert("Escolha um produto...");
+  } else {
+    boraZap = confirm(
+      "Você será redirecionado para o nosso WhatsApp. Tudo bem?"
+    );
+    vaiNoZap();
+  }
+}
+
+function vaiNoZap() {
+  if (boraZap == true) {
+    window.open("https://wa.me/5581995097377?text=" + mensagem);
+  } else {
+    console.log("não aceitou whatsapp");
+  }
 }
